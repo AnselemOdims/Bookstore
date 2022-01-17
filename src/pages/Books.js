ed3c2d3d3ced5c4f5a67a87262c3ceaf5a7f44f0
Anselem <anselemodims@gmail.com>
2022-01-17 14:25:26 +0100

    Feat: built out the Books pages component

---
+++ src/pages/Books.js
@@ -0,0 +1,11 @@
+import BookContainer from '../components/BookContainer';
+import InputBook from '../components/InputBook';
+
+const Books = () => (
+  <div>
+    <BookContainer />
+    <InputBook />
+  </div>
+);
+
+export default Books;
