86af8c8944b96f55ab254ec7640d91a1fb37fe8f
Anselem <anselemodims@gmail.com>
2022-01-17 14:37:40 +0100

    Feat: included the Router and Links to enable single page routing

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,13 @@
+/* eslint-disable react/button-has-type */
+import PropTypes from 'prop-types';
+
 const Button = ({ type, text }) => (
   <button type={type}>{text}</button>
 );
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Books from './pages/Books';
 import Categories from './pages/Categories';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Books />
-    <Categories />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
