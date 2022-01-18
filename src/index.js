c580ff951ca62c0768d000d43dc0a54b0e96cfd7
Anselem <anselemodims@gmail.com>
2022-01-18 09:52:41 +0100

    Feat: imported the Provider function and added the created store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './redux/configureStore';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
 import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
 
+// combine one or more reducers
 const reducer = combineReducers({
   booksReducer,
 });
 
+// create the store
 const store = createStore(reducer, applyMiddleware(logger));
 
 export default store;
