28c9d19aaf41a188aea83561c23b0a14ad2c07d0
Anselem <anselemodims@gmail.com>
2022-01-18 09:41:29 +0100

    Feat: declared the initialState and assigned it to an empty array

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -7,17 +7,20 @@ const BOOK_REMOVED = 'bookStore/books/BOOK_REMOVED';
  * @param {Object} payload - the payload for adding a new Book
  * @returns {Object} - the action object
  */
-export const addBook = payload => ({
+export const addBook = (payload) => ({
   type: BOOK_ADDED,
   payload,
 });
 
 /**
  * @function removeBook - the action creator for removing a book
- * @param {Object} payload - the payload for adding a new Book
+ * @param {Object} payload - the payload for removing a Book
  * @returns {Object} - the action object
  */
-export const removeBook = payload => ({
+export const removeBook = (payload) => ({
   type: BOOK_REMOVED,
   payload,
 });
+
+// declare the initial state
+const initialState = [];
