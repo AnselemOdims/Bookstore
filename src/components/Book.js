e9099e0b65714694f31d9afcbcbc2d267fa2a389
Anselem <anselemodims@gmail.com>
2022-01-18 10:05:02 +0100

    Feat: installed the react-circular-progress package and imported it

---
+++ src/components/Book.js
@@ -0,0 +1,4 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
