354188437314dbf0a4f37fa7624a4ba0ecba25ad
Anselem <anselemodims@gmail.com>
2022-01-19 00:37:17 +0100

    Feat: style the Book component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 const StyledButton = styled.button`
   width: 11.5rem;
-  height: 2.813rem;
-  padding: 0.801rem 1.188rem 0.886rem 1.375rem;
+  height: ${(props) => (props.update === 'update' ? '2.063rem' : '2.813rem')};
+  padding: ${(props) => (props.update === 'update' ? '0.438rem 1.188rem 0.5rem 1.375rem' : '0.801rem 1.188rem 0.886rem 1.375rem')};
   border-radius: 3px;
   font-family: var(--font-2);
   background: var(--primary-color-1);
@@ -17,13 +17,18 @@ const StyledButton = styled.button`
   }
 `;
 
-const Button = ({ text, handleClick }) => (
-  <StyledButton type="button" onClick={handleClick}>{text}</StyledButton>
+const Button = ({ text, handleClick, update }) => (
+  <StyledButton type="button" onClick={handleClick} update={update}>{text}</StyledButton>
 );
+
+Button.defaultProps = {
+  update: 'something',
+};
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
+  update: PropTypes.string,
 };
 
 export default Button;
