deb50f5d09e5e11977c029b03523eb83452e468f
Anselem <anselemodims@gmail.com>
2022-01-18 09:47:27 +0100

    Feat: created the books reducer function and exported it

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -24,3 +24,22 @@ export const removeBook = (payload) => ({
 
 // declare the initial state
 const initialState = [];
+
+/**
+ * @function booksReducer - the reducer function for books
+ * @param {Array} state - the current state
+ * @param {Object} param1 - the action object
+ * @returns {Array} - the current state array
+ */
+const booksReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case BOOK_ADDED:
+      return [...state, payload];
+    case BOOK_REMOVED:
+      return (state.filter(({ id }) => id !== payload.id));
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
